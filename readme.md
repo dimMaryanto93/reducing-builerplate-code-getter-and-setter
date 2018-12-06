da2bffeb9c3c93235fda566375f8098322ca18a2
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-12-06 16:39:10 +0700

    init project

---
+++ readme.md
@@ -0,0 +1,1 @@
+mvn archetype:generate -Dversion=1.0 -DgroupId=com.maryanto.dimas.example -DartifactId=reducing-builderplate-code -DarchetypeCatalog='Internal'
---
+++ src/main/java/com/maryanto/dimas/example/App.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
